b296f03e0167f87c319e67d6165162bdd03002d7
me-mayank <174928744+me-mayank@users.noreply.github.com>
2025-09-04 16:10:16 +0530

    fixed import issues

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { createReport } from "../controllers/reports.controllers";
-import { protect } from "../middlewares/auth.middleware";
+import { createReport } from "../controllers/reports.controllers.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
