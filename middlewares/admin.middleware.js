f54a7c7c961135e0b2e681a4df045fedfa24b878
me-mayank <174928744+me-mayank@users.noreply.github.com>
2025-09-06 06:07:57 +0530

    fixed delete user fns

--- middlewares/admin.middleware.js
+++ middlewares/admin.middleware.js
@@ -1,6 +1,6 @@
 export const adminOnly = async (req, res, next) => {
-  if (req.user.role !== "admin") {
-    res.status(403).json({
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({
       message: "Admin access is required to go further !!",
     });
   }
